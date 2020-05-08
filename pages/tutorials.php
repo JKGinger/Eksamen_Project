6f768a511e657996441615a1fe7ea00c9972db2f
simgortz <business@simgortz.com>
2020-05-08 11:48:47 +0200

    test

---
+++ pages/tutorials.php
@@ -0,0 +1,110 @@
+
+    <header>
+      <h1 class="overskift">Tutorials</h1>
+      <h2 class="mellemskrift">Survival gamemode</h2>
+    </header>
+
+    <div class="container_vid vid2">
+      <div class="row justify-content-center mb-3">
+        <div class="col-lg-5 embed-responsive embed-responsive-16by9 p-3 mx-3">
+            
+          <iframe
+          class="embed-responsive-item"
+          src="https://www.youtube.com/embed/UtLun2-jOQ8"
+          width="520"
+          height="315"
+          src="/"
+          frameborder="0"
+          allowfullscreen
+        >
+        </iframe>
+      </div>
+    
+        <div class="col-lg-5 videoforklaring order-1 order-lg-12 p-3 mx-3">
+          <h2>Survive the mine</h2>
+        <p>
+          Lorem ipsum dolor sit amet, nec in, vel
+          arcu etiam in leo, malesuada ligula. Dui dictumst montes pulvinar,
+          nonummy imperdiet, tincidunt mauris nulla felis erat, sodales semper,
+          est suscipit lorem mauris. Ac commodo varius neque, nulla lorem,
+          praesent est semper ut ut. Tempor tempus euismod. Leo vestibulum a. Ac commodo varius neque, nulla lorem,
+          praesent est semper ut ut. Tempor tempus euismod. Leo vestibulu,arcu etiam in leo, malesuada ligula. Dui dictumst montes pulvinar,
+          nonummy imperdiet, tincidunt mauris nulla felis erat, sodales semper,
+          est suscipit lorem mauris. Ac commodo varius neque, nulla lorem,
+          praesent est semper ut ut. Tempor tempus euismod. Leo vestibulum a. Ac commodo varius neque, nulla lorem,
+          praesent est semper ut ut. Tempor t
+          eget
+        </p>
+    </div>
+    </div>
+
+
+  <div class="container_vid vid2">
+    <div class="row justify-content-center mb-3">
+    
+
+      <div class="col-lg-5 videoforklaring p-3 mx-3 order-12 order-lg-1">
+        <h2>Make a farm</h2>
+      <p>
+        Lorem ipsum dolor sit amet, nec in, vel
+        arcu etiam in leo, malesuada ligula. Dui dictumst montes pulvinar,
+        nonummy imperdiet, tincidunt mauris nulla felis erat, sodales semper,
+        est suscipit lorem mauris. Ac commodo varius neque, nulla lorem,
+        praesent est semper ut ut. Tempor tempus euismod. Leo vestibulum a. Ac commodo varius neque, nulla lorem,
+        praesent est semper ut ut. Tempor tempus euismod. Leo vestibulu,arcu etiam in leo, malesuada ligula. Dui dictumst montes pulvinar,
+        nonummy imperdiet, tincidunt mauris nulla felis erat, sodales semper,
+        est suscipit lorem mauris. Ac commodo varius neque, nulla lorem,
+        praesent est semper ut ut. Tempor tempus euismod. Leo vestibulum a. Ac commodo varius neque, nulla lorem,
+        praesent est semper ut ut. Tempor t
+        eget
+      </p>
+  </div>
+
+  <div class="col-lg-5 embed-responsive embed-responsive-16by9 p-3 mx-3 order-1 order-lg-12">
+      
+    <iframe
+      class="embed-responsive-item"
+      src="https://www.youtube.com/embed/UtLun2-jOQ8"
+      width="520"
+      height="315"
+      src="/"
+      frameborder="0"
+      allowfullscreen
+    >
+    </iframe>
+  </div>
+</div>
+
+<div class="container_vid vid2">
+  <div class="row justify-content-center mb-3">
+    <div class="col-lg-5 embed-responsive embed-responsive-16by9 p-3 mx-3">
+        
+      <iframe
+      class="embed-responsive-item"
+      src="https://www.youtube.com/embed/UtLun2-jOQ8"
+      width="520"
+      height="315"
+      src="/"
+      frameborder="0"
+      allowfullscreen
+    >
+    </iframe>
+  </div>
+
+    <div class="col-lg-5 videoforklaring order-1 order-lg-12 p-3 mx-3">
+      <h2>Survive the mine</h2>
+    <p>
+      Lorem ipsum dolor sit amet, nec in, vel
+      arcu etiam in leo, malesuada ligula. Dui dictumst montes pulvinar,
+      nonummy imperdiet, tincidunt mauris nulla felis erat, sodales semper,
+      est suscipit lorem mauris. Ac commodo varius neque, nulla lorem,
+      praesent est semper ut ut. Tempor tempus euismod. Leo vestibulum a. Ac commodo varius neque, nulla lorem,
+      praesent est semper ut ut. Tempor tempus euismod. Leo vestibulu,arcu etiam in leo, malesuada ligula. Dui dictumst montes pulvinar,
+      nonummy imperdiet, tincidunt mauris nulla felis erat, sodales semper,
+      est suscipit lorem mauris. Ac commodo varius neque, nulla lorem,
+      praesent est semper ut ut. Tempor tempus euismod. Leo vestibulum a. Ac commodo varius neque, nulla lorem,
+      praesent est semper ut ut. Tempor t
+      eget
+    </p>
+</div>
+</div>
